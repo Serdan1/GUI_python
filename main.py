903686d08a390e8c19604cb946029beb6bf4a386
Daniel Serrano Martin <danielserranoreal@gmail.com>
2025-04-04 07:54:00 +0000

    instalación pip install PyQt6

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from modelo import *
+from modelo import TorresDeHanoi
+import sys
+from PyQt6.QtWidgets import QApplication
 
-
+# Este es el punto de entrada de la aplicación
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
-    w = AppContador()
-    w.show()
-
+    ventana = TorresDeHanoi()
+    ventana.show()
     sys.exit(app.exec())
